24aa9bacb5251d304fe484fc06af6bb742b2dce2
alan-bolger <alan.j.bolger@gmail.com>
2020-09-28 11:36:39 +0100

    Initial commit.

---
+++ Assets/Tests/CubeFilterTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class CubeFilterTest
+    {
+        [Test]
+        public void CubeFilterSimple()
+        {
+            // Assert.IsTrue(false);
+
+            int[] input = { 1, 3, 7, 7, 9, 999 }; // Error
+            // int[] input = { 1, 3, 5, 7, 9, 999 }; // Correct
+            int[] output = CubeFilter.cubes(input);
+            int[] expected = { 1, 27, 125, 343, 729, 997002999 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
+
