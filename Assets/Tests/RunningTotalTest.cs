57952a1e98bf08cb673d165e236ef08e403c5f0f
alan-bolger <alan.j.bolger@gmail.com>
2020-10-05 11:54:10 +0100

    Added tests for negative values.

--- Assets/Tests/RunningTotalTest.cs
+++ Assets/Tests/RunningTotalTest.cs
@@ -13,9 +13,9 @@ namespace Tests
         [Test]
         public void RunningTotal_IO()
         {
-            int[] input = { 4, 2, 8, 3, 9, 4, 10, 5, 11, 6 };
+            int[] input = { 4, 2, 8, 3, 9, 4, 10, 5, 11, 6, -4, -1, 2, -6, -10, 7, -12, 5, -21 };
             int[] output = RunningTotal.adder(input);
-            int[] expected = { 4, 6, 14, 17, 26, 30, 40, 45, 56, 62 };
+            int[] expected = { 4, 6, 14, 17, 26, 30, 40, 45, 56, 62, 58, 57, 59, 53, 43, 50, 38, 43, 22 };
 
             CollectionAssert.AreEqual(expected, output);
         }
